78de522ddd31f5d8a0556933eb733c923b60a40d
Omadoye <dedeomadoye@yahoo.com>
2021-08-28 15:46:48 +0100

    app

---
+++ frontend/frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      
+    </Router>
+  );
+}
+
+export default App;
