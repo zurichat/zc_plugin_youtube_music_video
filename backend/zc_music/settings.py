bb85dd49a423f77373e85f0863674857e9a2dc06
Israel Aire <airenov500@gmail.com>
2021-08-30 08:20:31 +0100

    Update settings.py
    If the env is in production, set https redirect to true

--- backend/zc_music/settings.py
+++ backend/zc_music/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +125,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Redirect automatically to https if the environment has a variable called true
+if os.getenv("ENV") != "DEV":
+    SECURE_SSL_REDIRECT = True
+    
