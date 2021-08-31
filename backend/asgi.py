936907f6019c52239244dfa5d0fa1655216bcb83
Chuckz Okoye <tricelex@gmail.com>
2021-08-31 20:12:30 +0100

    Initialize Repo

--- backend/asgi.py
+++ backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for zc_music project.
+ASGI config for backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zc_music.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_asgi_application()
--- backend/urls.py
+++ backend/urls.py
@@ -1,4 +1,4 @@
-"""zc_music URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import index
+
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
 ]
