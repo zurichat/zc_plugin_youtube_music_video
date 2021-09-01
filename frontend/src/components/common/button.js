18b8b6b652efea0cb5be66b4133ab5496417c63a
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-01 16:48:40 +0100

    Frontend setup complete

---
+++ frontend/src/components/common/button.js
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+function Button() {
+  return <Wrapper>Add here</Wrapper>;
+}
+
+const Wrapper = styled.button`
+  padding: 5px 10px;
+  color: white;
+  background: green;
+  border: none;
+  outline: none;
+`;
+
+export default Button;
