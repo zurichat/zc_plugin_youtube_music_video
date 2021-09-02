a8571d6c0495c8e5f0467e3efa16fd22f6530359
Temi-Pinheiro <temitopepinheiro@gmail.com>
2021-09-02 21:56:31 +0100

    playlist test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,15 @@
-import React, { Component } from "react";
-import ReactDOM from "react-dom";
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Frontend Setup Test</h1>
-        <h3>Team Pythagoras</h3>
-        <h3>Team Pythagoras</h3>
-        <h3>Team Pythagoras</h3>
-        <h3>Team Pythagoras</h3>
-        <h3>Team Pythagoras</h3>
+        <h3>Team Temi</h3>
       </div>
     );
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById('app'));
