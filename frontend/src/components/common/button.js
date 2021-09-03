1f537fb3997f3dd71a70de0d7f3bacdb0627c1cc
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2021-09-03 16:21:32 +0100

    merged

---
+++ frontend/src/components/common/button.js
@@ -0,0 +1,51 @@
+/*
+BUTTON  HTML
+<Button class="primary"><div>Add a song to the playlist</div></Button>
+<Button class="secondary"><div>Play</div></Button>
+
+BUTTON CSS
+.primary{
+  font-size: 16px;
+  width: 13.25em;
+  height: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: #00B87C;
+  box-shadow: 2px 2px 2px rgba(0, 36, 24, 0.04);
+  border-radius: 3px;
+  border-color: #00B87C;
+  border-style: solid;
+}
+
+.primary div{
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  color: #FFFFFF;
+}
+
+.secondary{
+  font-size: 16px;
+  width: 3.9375em;
+  height: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background: #FFFFFF;
+  border: 1.5px solid #00B87C;
+  border-radius: 3px;
+}
+.secondary div{
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  color: #00B87C;
+}
+*/
---
+++ frontend/src/components/common/chatHeader.js
@@ -0,0 +1,56 @@
+//chatHeader style
+/**
+ *
+      @import url("https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap");
+      * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        color: #00b87c;
+      }
+      .chatHeader {
+        display: flex;
+        flex-direction: row;
+        width: 347px;
+        height: 64px;
+        padding: 16px;
+        justify-content: space-between;
+        align-items: flex-start;
+        font-family: "Lato", sans-serif;
+        border: 1.22693px solid #00b87c;
+        position: static;
+      }
+      .chatHeader__left {
+        display: flex;
+        align-items: center;
+      }
+      .message {
+        width: 13.5px;
+        height: 12.55px;
+      }
+
+      .chatHeader__left p {
+        font-weight: bold;
+        font-size: 15px;
+        margin-left: 12px;
+      }
+      .close {
+        cursor: pointer;
+        width: 24px;
+        height: 24px;
+        padding: 3px;
+        background: linear-gradient(0deg, #e5fff6, #e5fff6);
+      }
+ */
+
+//chatHeader HTML
+/**
+  * <div class="chatHeader">
+      <div class="chatHeader__left">
+        <i class="far fa-comment-alt message"></i>
+        <p>Chat</p>
+      </div>
+      <div class="chatHeader__right"></div>
+      <i class="fas fa-times close"></i>
+    </div>
+  */
