8bc090bee9f586aab8d3e24755c9b2c8b8ee54b7
Irene Boyle <ogbeweoghomwen@yahoo.com>
2021-09-03 16:43:36 +0100

    Convert Like button to react

--- frontend/src/components/common/like.js
+++ frontend/src/components/common/like.js
@@ -17,3 +17,47 @@
 
 //html for heart/likes icon
 <i class="far fa-heart"></i>
+
+import React, {useState} from 'react'
+
+
+
+const Like = () => {
+    const [like,setLike] = useState(false)
+    const handleLike = () =>{
+        setLike(!like)
+    }
+    const styles = {
+        heartLike:{
+            color: '#4ECB71',
+            height: '20px',
+            width: '20px',
+            left: '2.2890625px',
+            top: '2px',
+            borderRadius: '0px',
+            cursor: 'pointer',
+        
+        },
+        emptyHeart:{
+            height: '20px',
+            width: '20px',
+            left: '2.2890625px',
+            top: '2px',
+            borderRadius: '0px',
+            cursor: 'pointer',
+        
+        }
+    }
+       
+      
+    return (
+
+        <div>
+            {like?<i class="fas fa-heart" style={styles.heartLike} onClick={handleLike}></i>:<i class="far fa-heart"  style={styles.emptyHeart} onClick={handleLike} ></i>}
+            
+        </div>
+    )
+}
+
+export default Like
+
