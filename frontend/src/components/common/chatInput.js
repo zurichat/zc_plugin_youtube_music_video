b7db8019c77546cef05b960ee83c5ca0f4310921
Ajibola Akindunbi <jibzsupreme@gmail.com>
2021-09-03 16:48:29 +0100

    Issues 255, 256, 257-chatInput with styled component

--- frontend/src/components/common/chatInput.js
+++ frontend/src/components/common/chatInput.js
@@ -1,6 +1,6 @@
-import icon1 from './icon1.svg';
-import icon2 from './icon2.svg';
-import icon3 from './icon3.svg';
+import icon1 from './media/icon1.svg';
+import icon2 from './media/icon2.svg';
+import icon3 from './media/icon3.svg';
 
 import styled from 'styled-components'
 
