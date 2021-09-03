6746eca96315e08e66017a277eac97d51d02b64e
Prudence2020 <uzoekwelevisonc@gmail.com>
2021-09-03 04:33:15 -0700

    committing playlistitems html and css

---
+++ frontend/src/components/common/playlistItem.js
@@ -0,0 +1,447 @@
+// // <!DOCTYPE html>
+// // <html lang="eng">
+// //     <head>
+// //         <meta charset=" UTF-8">
+// //         <meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0">
+// //         <link rel="stylesheet" type="text/css" href="style.css">
+        
+// //         <title> Music Plugin Playlist Item</title>
+// //     </head>
+// //     <body>
+// //         <section id="playlist">
+// //             <div class="container">
+// //             <img  class="img" src="img/Rectangle 183.png">
+// //             <div class="playlistclass">
+// //             <h3>Essence (ft. Tems) - Wizkid</h3>
+// //             <p>Added by smoothice</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >4. 05 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p >642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (11).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+
+// //         <section id="playlist1">
+// //             <div class="container-one">
+// //             <img  class="img" src="img/Rectangle 184.png">
+// //             <div class="playlistclass">
+// //             <h3>Ade Ori (Audio) - Tiwa Savage</h3>
+// //             <p>Added by Tosingh54</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >3. 52 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p>642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (13).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+
+// // <section id="playlist1">
+// //             <div class="container-one">
+// //             <img  class="img" src="img/Rectangle 185.png">
+// //             <div class="playlistclass">
+// //             <h3>Wade in the Water - Eva Cassidy</h3>
+// //             <p>Added by Pauline. B</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >2. 25 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p>642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (13).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+
+// //         <section id="playlist1">
+// //             <div class="container-one">
+// //             <img  class="img" src="img/Rectangle.png">
+// //             <div class="playlistclass">
+// //             <h3>Start a Fire - Passenger</h3>
+// //             <p>Added by Liliand</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >4. 25 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p>642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (13).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+
+// //         <section id="playlist1">
+// //             <div class="container-one">
+// //             <img  class="img" src="img/miguel.png">
+// //             <div class="playlistclass">
+// //             <h3>She’s like a Rose - Miguel</h3>
+// //             <p>Added by Imhade</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >3. 37 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p>642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (13).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+// //         <section id="playlist1">
+// //             <div class="container-one">
+// //             <img  class="img" src="img/Rectangle 185(3).png">
+// //             <div class="playlistclass">
+// //             <h3>Hello Goodbye (ft. Sir Dauda) - Ladipoe</h3>
+// //             <p>Added by Lucky</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >3. 52 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p>642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (13).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+// //         <section id="playlist1">
+// //             <div class="container-one">
+// //             <img  class="img" src="img/Rectangle 185(4).png">
+// //             <div class="playlistclass">
+// //             <h3>Welcome (ft. Laycon) - June muzz</h3>
+// //             <p>Added by Imhade</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >3. 47 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p>642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (13).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+// //         <section id="playlist1">
+// //             <div class="container-one">
+// //             <img  class="img" src="img/Rectangle 185(6).png">
+// //             <div class="playlistclass">
+// //             <h3>No Fit Vex - Burna Boy</h3>
+// //             <p>Added by Imhade</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >3. 59 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p>642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (13).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+// //         <section id="playlist1">
+// //             <div class="container-one">
+// //             <img  class="img" src="img/Rectangle 185 (1).png">
+// //             <div class="playlistclass">
+// //             <h3>Cant relate - Omah Lay</h3>
+// //             <p>Added by Imhade</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >2. 59 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p>642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (13).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+// //         <section id="playlist1">
+// //             <div class="container-one">
+// //             <img  class="img" src="img/Rectangle 185 (2).png">
+// //             <div class="playlistclass">
+// //             <h3>She’s like a Rose - Miguel</h3>
+// //             <p>Added by Imhade</p>
+// //         </div>
+// //         <div class="para">
+// //             <p >2. 59 mins</p>
+// //         </div>
+// //         <div class="paragraph">
+// //             <p>642 likes</p>
+// //         </div>
+// //         <div class="vector">
+// //             <img   class="vector1" src="img/Vector (13).png">
+// //             <img  class="vector2" src="img/Vector (12).png">
+// //         </div>
+// //         </div>
+// //         </section>
+// //     </body>
+// // </html>
+// @import url('https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,300;0,400;0,700;0,900;1,300&display=swap');
+// * {
+// 	padding: 0;
+// 	margin: 0;
+// 	box-sizing: border-box;
+// 	text-decoration: none;
+// 	font-family: lato;	
+// }
+// .container{
+// 	margin-top: 3em !important;
+// 	display: grid;
+//     grid-template-columns:14% 52% 20% 20%;
+// 	align-items: center;
+// 	padding-right: 1em;
+// 	padding-left: 1em;
+// 	padding-top: 2em;
+// 	width: 100%;
+// 	margin: auto;
+// 	box-sizing: border-box;
+//     box-shadow: 0 5px 10px rgba(0, 0, 0,0.2);
+
+// }
+// .para{
+// 	display: none;
+// }
+// .container-one{
+// 	display: grid;
+//     grid-template-columns:14% 52% 20% 20%;
+// 	align-items: center;
+// 	padding-right: 1em;
+// 	padding-left: 1em;
+// 	padding-top: 2em;
+// 	width: 100%;
+// 	margin: auto;
+// 	box-sizing: border-box;
+//     box-shadow: 0 5px 10px rgba(0, 0, 0,0.2);
+// }
+// .playlistclass{
+// 	padding-bottom: 1em;
+// }
+// .playlistclass h3{
+
+// 	font-family: 'Lato';
+//     font-size: 12px;
+// font-style: normal;
+// font-weight: 700;
+
+// letter-spacing: 0px;
+// text-align: left;
+
+
+    
+// }
+// .playlistclass p{
+// 	font-size: 8px;
+// 	line-height: 2em;
+// 	font-weight: 400;
+
+// 	font-family: 'Lato';
+// font-size: 10px;
+// font-style: normal;
+// font-weight: 500;
+// line-height: 20px;
+// letter-spacing: 0px;
+// text-align: left;
+
+
+
+// }
+// .img{
+// 	width: 40px;
+// padding-bottom: 1em;
+	
+// }
+// .paragraph p{
+// 	font-size: 9px;
+// 	padding-right: 3em;
+//     padding-bottom: 1em !important;
+	
+// }
+// .vector1{
+// 	padding-right: 2em;
+//     padding-bottom: 1em;
+// width: 43px;
+// }
+// .vector2{
+//     padding-bottom: 1em;
+	
+// }
+// .vector11{
+// 	width: 10px;
+	
+// }
+// @media(min-width: 768px){
+// 	body{
+// 		width: 50%;
+// 		margin: auto;
+// 	}
+// 	.para{
+// 		display: none;
+// 	}
+	
+// }
+// @media(min-width: 1024px){
+// 	body{
+// 		width: 75%;
+// 		margin: auto;
+// 	}
+
+// 	.playlistclass h3{
+// 		font-size: 14px;
+			
+// 	}
+// 	.playlistclass p{
+// 		font-size: 12px;
+// 		line-height: 2.5em;
+		
+// 	}
+// 	.para{
+// 		display: block;
+// 	}
+// 	.para p{
+// 		padding-bottom: 1em ;
+// 		font-size: 14px;
+		
+// 	}
+// 	.paragraph p{
+// 		font-size: 12px;
+// 	}
+// 	.container{
+// 		margin-top: 3em !important;
+// 		display: grid;
+// 		grid-template-columns:10% 45% 15% 15% 40%;
+// 		align-items: center;
+// 		padding-right: 1em;
+// 		padding-left: 1em;
+// 		padding-top: 2em;
+// 		width: 100%;
+// 		margin: auto;
+// 		box-sizing: border-box;
+// 		box-shadow: 0 5px 10px rgba(0, 0, 0,0.2);
+	
+// 	}
+// 	.container-one{
+// 		display: grid;
+// 	grid-template-columns: 10% 45% 15% 15% 40%;
+// 		align-items: center;
+// 		padding-right: 1em;
+// 		padding-left: 1em;
+// 		padding-top: 2em;
+// 		width: 100%;
+// 		margin: auto;
+// 		box-sizing: border-box;
+// 		box-shadow: 0 5px 10px rgba(0, 0, 0,0.2);
+// 	}
+// 	.img{
+// 		width: 60px;
+	
+		
+// 	}
+// 	.vector1{
+// 		padding-right: 2em;
+// 		padding-bottom: 1em;
+// 		width: 50px;
+// 	}
+// 	.vector2{
+// 		padding-bottom: 1em;
+// 		width: 20px;
+// 		padding-right: 0.5em;
+// 	}
+
+// }
+// @media(min-width: 1440px){
+// 	body{
+// 		width: 70%;
+// 		margin: auto;
+// 	}
+// 	.playlistclass h3{
+// 		font-size: 20px;
+			
+// 	}
+// 	.playlistclass p{
+// 		font-size: 15px;
+// 		line-height: 2.5em;
+		
+		
+// 	}
+// 	.para{
+// 		display: block;
+// 	}
+// 	.para p{
+		
+// 		padding-bottom: 1em;
+// 		font-size: 16px;
+// 	}
+// 	.paragraph p{
+// 		font-size: 16px;
+// 	}
+// 	.container{
+// 		margin-top: 3em !important;
+// 		display: grid;
+// 		grid-template-columns:8% 45% 15% 15% 40%;
+// 		align-items: center;
+// 		padding-right: 1em;
+// 		padding-left: 1em;
+// 		padding-top: 2em;
+// 		width: 100%;
+// 		margin: auto;
+// 		box-sizing: border-box;
+// 		box-shadow: 0 5px 10px rgba(0, 0, 0,0.2);
+	
+// 	}
+// 	.container-one{
+// 		display: grid;
+// 	grid-template-columns: 8% 45% 15% 15% 40%;
+// 		align-items: center;
+// 		padding-right: 1em;
+// 		padding-left: 1em;
+// 		padding-top: 2em;
+// 		width: 100%;
+// 		margin: auto;
+// 		box-sizing: border-box;
+// 		box-shadow: 0 5px 10px rgba(0, 0, 0,0.2);
+// 	}
+// 	.img{
+// 		width: 60px;
+	
+		
+// 	}
+// 	.vector1{
+// 		padding-right: 2em;
+// 		padding-bottom: 1em;
+// 		width: 50px;
+// 	}
+// 	.vector2{
+// 		padding-bottom: 1em;
+// 		width: 20px;
+// 		padding-right: 0.5em;
+// 	}
+
+// }
