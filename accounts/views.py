c40e66d549f57d63ecb6e49a5a67b2b8cf07a222
omotolani12 <omotolaniosems@gmail.com>
2021-09-04 02:19:42 +0100

    worked on view.py for songs model

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,17 @@
 from django.http import JsonResponse
 from django.views import View
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Songs
+from django.db.models import query
+from rest_framework import generics, serializers, status
+from .serializers import SongsSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import mixins
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.renderers import JSONRenderer
 
 class PluginInfo(View):
 
@@ -15,3 +26,10 @@ class PluginInfo(View):
             "sidebar_url": "music.zuri.chat/sidebar",
         }
         return JsonResponse(data)
+
+        # Create your views here
+class SongsView(generics.ListAPIView):
+
+    serializer_class =  SongsSerializer
+    queryset = Songs.objects.all()
+    # print(dir( serializer_class ))
