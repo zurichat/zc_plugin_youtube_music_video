5ae6c478646b0de9d2510d3f43d5acfb4e82bbb6
Kolapo Damola Usman <damolausmankolapo@gmail.com>
2021-09-04 09:29:34 +0100

    styled button with styled components

--- frontend/src/components/common/button.js
+++ frontend/src/components/common/button.js
@@ -1,3 +1,4 @@
+@import url("https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap");
 import styled from "styled-components"
 
 function Button(props) {
@@ -12,21 +13,51 @@ function Button(props) {
 
 const Buttons = styled.button`
 &.primary{
-
+  font-size: 16px;
+  width: 13.25em;
+  height: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #00B87C;
+  box-shadow: 2px 2px 2px rgba(0, 36, 24, 0.04);
+  border-radius: 3px;
+  border-color: #00B87C;
+  border-style: solid;
+  cursor: pointer;
 }
 
 &.secondary{
-
+  font-size: 16px;
+  width: 3.9375em;
+  height: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: #FFFFFF;
+  border: 1.5px solid #00B87C;
+  border-radius: 3px;
+  cursor: pointer;
 }
 `
 
 const Div = styled.div`
 &.primary{
-
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  color: #FFFFFF;
 }
 
 &.secondary{
-
+  font-family: Lato;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 16px;
+  line-height: 24px;
+  color: #00B87C;
 }
 `
 
