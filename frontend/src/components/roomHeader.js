f431b65fa81578d96620dc9e193e266988cc1be6
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-04 08:05:15 +0100

    Testing site - Justice II

--- frontend/src/components/roomHeader.js
+++ frontend/src/components/roomHeader.js
@@ -16,7 +16,7 @@ const roomHeader = () => {
           className="header-group-icon"
         />
         <Link to="/" className="header-link">
-          Music Roomss
+          Music Room
         </Link>
       </div>
 
