4a6337ff5d6674d91cc70b82c516dbf52d1ec390
Amara Thaddy <stevellathaddy@gmail.com>
2021-09-04 19:01:09 +0100

    Views for the comment section in Youtube Plugin

--- accounts/views.py
+++ accounts/views.py
@@ -1,64 +1,83 @@
-from rest_framework.decorators import api_view
+from django.shortcuts import render
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 
-from .api import Comments
+from .api import Comment
 from .serializers import CommentSerializer
 
 #A dictionary created for the sake of passing dummy data
-# actual data would be gottrn from the database when connected
+# actual data would be gotten from the database when connected
 
 comments = {
-	1: Comments(id =1, username = 'Joseph', created_at = , message= 'I like this music')
-	2: Comments(id =2, username = 'Amara', created_at = , message= 'This music is dope')
-	3: Comments(id =3, username = 'Ken', created_at = , message= 'This track is something else')
+    1: Comment(id=1, username = 'Joseph', created_at = '30 minutes ago' , message= 'I like this music'),
+    2: Comment(id=2, username = 'Amara', created_at = '35 minutes ago', message= 'This music is dope'),
+    3: Comment(id=3, username = 'Ken', created_at ='40 minutes ago' , message= 'This track is something else'),
 }
 
+class CommentViewSet(viewsets.ViewSet):
+    serializer_class = CommentSerializer
 
-@api_view( ['GET', 'POST'] )
-def Comment_list(request):
-	'''List all comments or create a new comment'''
-	if request.method == 'GET':
-		serializer = CommentSerializer(instance = comments.values(), many=True)
-		return Response(serializer.data)
+    def list(self,request):
+        serializer = CommentSerializer(instance = comments.values(), many=True)
+        return Response(serializer.data)
 
-	elif request.method == 'POST':
-		serializer = CommentSerializer(data=request.data)
-		if serializer.is_valid():
-			comments[max(comments)+1] = serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-		
+    def create(self, request):
+        serializer = CommentSerializer(data= request.data)
+        if serializer.is_valid():
 
-@api_view( ['GET', 'PUT','PATCH'] )
-def Comment_Detail(request, pk):
-    '''Retrieve, update or Comment Instance'''
-    try:
-    	comment = comments[int(pk)]
-    except KeyError:
-    	return Response(status= status.HTTP_404_NOT_FOUND)
-	except ValueError:
-    	return Response(status= status.HTTP_400_BAD_REQUEST)
+            comments[max(comments)+1] = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
- 	if request.method == 'GET':
- 		serializer = CommentSerializer(instance=comment)
- 		return Response(serializer.data)
+    def retrieve(self, request, pk=None):
+        try:
+            comment = comments[int(pk)]
+        except KeyError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except ValueError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
-	elif request.method == 'PUT':
-		serializer = CommentSerializer( instance = comment, data= request.data)
+        serializer = CommentSerializer(instance=comment)
+        return Response(serializer.data)
 
-		if serializer.is_valid():
-			comments[comment.id] = serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-	elif request.method == 'PATCH':
-		serializer = CommentSerializer (instance = comment, 
-										data = request.data, partial =True)
-		if serializer.is_valid():
-			comments[comment.id] = serializer.save()
-			return Response(serializer.data, status=status.HTTP_201_CREATED)
-		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def update(self, request, pk=None):
+        try:
+            comment = comments[int(pk)]
+        except KeyError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except ValueError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = CommentSerializer( instance = comment, data= request.data)
+        if serializer.is_valid():
+            comments[max(comments)+1] = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        try:
+            comment = comments[int(pk)]
+        except KeyError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except ValueError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = CommentSerializer( instance = comment, data= request.data, partial=True)
+        if serializer.is_valid():
+            comments[max(comments)+1] = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+
+
+
+
+
+    
+        
 
 
 
