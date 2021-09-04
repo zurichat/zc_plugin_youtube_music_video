32dcc56376791f45f0cc6050b713a08bfdef1ed2
thenedum <74463502+thenedum@users.noreply.github.com>
2021-09-04 09:13:36 -0700

    Styled button with styled-components - issue 283

--- frontend/src/components/common/button.js
+++ frontend/src/components/common/button.js
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { injectGlobal } from "styled-components";
 
 function Button(props) {
   let classes = "primary"
@@ -10,42 +11,23 @@ function Button(props) {
          </Buttons>;
 }
 
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap');
+  
+  body {
+    font-family: 'Lato', sans-serif;
+  }
+`
+
 const Buttons = styled.button`
 &.primary{
-
-}
-
-&.secondary{
-
-}
-`
-
-const Div = styled.div`
-&.primary{
-
-}
-
-&.secondary{
-
-}
-`
-
-export default Button;
-
-/*
-BUTTON  HTML
-<Button class="primary"><div>Add a song to the playlist</div></Button>
-<Button class="secondary"><div>Play</div></Button>
-
-BUTTON CSS
-.primary{
   font-size: 16px;
   width: 13.25em;
   height: 3em;
   display: flex;
   align-items: center;
   justify-content: center;
-
+  cursor: pointer;
   background: #00B87C;
   box-shadow: 2px 2px 2px rgba(0, 36, 24, 0.04);
   border-radius: 3px;
@@ -53,7 +35,22 @@ BUTTON CSS
   border-style: solid;
 }
 
-.primary div{
+&.secondary{
+  font-size: 16px;
+  width: 3.9375em;
+  height: 3em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+  background: #FFFFFF;
+  border: 1.5px solid #00B87C;
+  border-radius: 3px;
+}
+`
+
+const Div = styled.div`
+&.primary{
   font-family: Lato;
   font-style: normal;
   font-weight: bold;
@@ -62,19 +59,7 @@ BUTTON CSS
   color: #FFFFFF;
 }
 
-.secondary{
-  font-size: 16px;
-  width: 3.9375em;
-  height: 3em;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  background: #FFFFFF;
-  border: 1.5px solid #00B87C;
-  border-radius: 3px;
-}
-.secondary div{
+&.secondary{
   font-family: Lato;
   font-style: normal;
   font-weight: bold;
@@ -82,4 +67,7 @@ BUTTON CSS
   line-height: 24px;
   color: #00B87C;
 }
-*/
+`
+
+export default Button;
+
