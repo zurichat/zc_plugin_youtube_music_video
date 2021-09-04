1629068c66fcf3af799a527eeeca60bf5da1ae5e
John Danlami <35366478+johnsmccain@users.noreply.github.com>
2021-09-04 14:09:45 +0100

    Update playlist.js
    Connecting playlists to playlist item

--- frontend/src/components/playlist.js
+++ frontend/src/components/playlist.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-
+import PlaylistItem from './common/playlistItem';
 import PlaylistHeader from "./common/playlistHeader";
 
 function Playlist() {
   return (
     <Wrapper>
       <PlaylistHeader />
+      <PlaylistItem/>
     </Wrapper>
   );
 }
