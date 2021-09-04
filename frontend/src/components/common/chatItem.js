f73cc6054fa2da8c9a1be0cc11454e0a57dc74c9
Temi-Pinheiro <temitopepinheiro@gmail.com>
2021-09-04 02:50:55 +0100

    PlaylistItem Styled Components

--- frontend/src/components/common/chatItem.js
+++ frontend/src/components/common/chatItem.js
@@ -1,41 +1,14 @@
-<<<<<<< HEAD
+import React from 'react';
 import styled from 'styled-components';
-
-const ChatItem = () => {
-  return (
-    <div>
-      <Layout>
-        <CItem>
-          <CItemImg>
-            <img src='src/media/People of Brooklyn Avatar2.png' alt='People' />
-          </CItemImg>
-          <Right>
-            <Name>
-              <h3>Amara</h3>
-              <p>23 minutes ago</p>
-            </Name>
-            <ChatText>
-              <p>If you ask me i would say it's overrated but...</p>
-            </ChatText>
-          </Right>
-        </CItem>
-      </Layout>
-    </div>
-  );
-};
-=======
-import React from "react";
-import styled from "styled-components";
 // import image from "src/media/user.png";
->>>>>>> 561bd2228154eaaee8cc988eddfc591ca688baad
 
 function ChatItem(props) {
   return (
     <Chat>
       <Items>
         <Img
-          src="https://images.unsplash.com/photo-1498462440456-0dba182e775b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3BsYXNofGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-          alt="user avater"
+          src='https://images.unsplash.com/photo-1498462440456-0dba182e775b?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8c3BsYXNofGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+          alt='user avater'
         />
         <NameTime>
           <Describe>
@@ -51,149 +24,6 @@ function ChatItem(props) {
 
 export default ChatItem;
 
-<<<<<<< HEAD
-//             <div class="chatitem-img">
-//                 <img src="People.png" alt="People">
-//             </div>
-
-//             <div class="right">
-
-//                 <div class="name">
-//                     <h3>Amara</h3>
-//                     <p>23 minutes ago</p>
-//                 </div>
-
-//                 <div class="text">
-//                     <p>If you ask me, I would say it is so overrated, but...</p>
-//                 </div>
-//             </div>
-//         </div>
-
-//     </div>
-
-// </body>
-// </html>
-
-// html {
-//     font-size: 100%;
-//   }
-
-//   @media (max-width: 1180px){
-//     html{
-//       font-size: 98%;
-//     }
-//   }
-//   @media (max-width: 1080px){
-//     html{
-//       font-size: 88%;
-//     }
-//   }
-//   @media (max-width: 980px){
-//     html{
-//       font-size: 79%;
-//     }
-//   }
-//   @media (max-width: 850px){
-//     html{
-//       font-size: 69%;
-//     }
-//   }
-//   @media (max-width: 700px){
-//     html{
-//       font-size: 58%;
-//     }
-//   }
-//   @media (max-width: 600px){
-//     html{
-//       font-size: 50%;
-//     }
-//   }
-
-//   @media (min-width: 1300px){
-//     html{
-//       font-size: 130%;
-//     }
-//   }
-//   @media (min-width: 1500px){
-//     html{
-//       font-size: 150%;
-//     }
-//   }
-//   @media (min-width: 1700px){
-//     html{
-//       font-size: 175%;
-//     }
-//   }
-//   @media (min-width: 2000px){
-//     html{
-//       font-size: 189%;
-//     }
-//   }
-//   @media (min-width: 2400px){
-//     html{
-//       font-size: 200%;
-//     }
-//   }
-//   @media (min-width: 2700px){
-//     html{
-//       font-size: 210%;
-//     }
-//   }
-//   @media (min-width: 2880px){
-//     html{
-//       font-size: 220%;
-//     }
-//   }
-//   @media (min-width: 3000px){
-//     html{
-//       font-size: 250%;
-//     }
-//   }
-
-// @import url('https://fonts.googleapis.com/css2?family=Lato:wght@400;900&display=swap');
-// * {
-//     padding: 0;
-//     margin: 0;
-//     box-sizing: border-box;
-// }
-// body {
-//     font-family: 'Lato', sans-serif;
-//     font-size: 1.2rem;
-//     /* display: flex;
-//     align-items: center;
-//     background-color: violet;
-//     min-height: 100vh; */
-// }
-// .layout {
-//     display: flex;
-//     align-items: center;
-//     min-height: 100vh;
-// }
-// .chat-item {
-//     display: flex;
-//     width: 29rem;
-//     background-color: #ffffff;
-//     margin: auto;
-//     padding-left: 1rem;
-//     text-align: center;
-// }
-// .chatitem-img {
-//     background-color: aqua;
-//     width: 5rem;
-//     height: 3.1rem;
-//     border-radius: 50%;
-//     margin: auto 3rem;
-//     margin-right: 1.2rem;
-//     padding-left: 0rem;
-//     padding-right: 0rem;
-
-// }
-//  img {
-//     width: 4rem;
-//     height: auto;
-//     margin:  0 auto;
-// }
-=======
 export const Chat = styled.div`
   width: 347px;
   height: 573px;
@@ -236,7 +66,6 @@ export const Span = styled.span`
   line-height: 19px;
   color: #c1c1c1;
 `;
->>>>>>> 561bd2228154eaaee8cc988eddfc591ca688baad
 
 export const P = styled.p`
   font-size: 14px;
@@ -256,45 +85,3 @@ export const NameTime = styled.div`
   height: 71px;
   margin: 0px 8px;
 `;
-
-<<<<<<< HEAD
-//     /* padding-right: 21rem; */
-// }
-// .text {
-//     color: #3A3A3A;
-//     padding-top: 1rem;
-//     font-size: 0.7rem;
-//     line-height: 1.2rem;
-//     width: 15rem;
-// }
-//  @media (max-width: 600px) {
-//      .chat-item {
-//          width: 6rem;
-//          height: auto;
-//      }
-//      .name {
-//         padding-bottom: 0.2rem;
-//      }
-//     .name h3 {
-//         font-size: 1.9rem;
-//     }
-//     .name p {
-//         font-size: 1.5rem;
-//         /* padding-bottom: 2rem; */
-//     }
-//     .text {
-//         font-size: 1rem;
-//     }
-// }
-// @media (max-width: 375px) {
-//     .chat-item {
-//         justify-content: center;
-
-//     }
-//     .text {
-//         font-size: 1.2rem;
-//         line-height: 1.5rem;
-//     }
-// }
-=======
->>>>>>> 561bd2228154eaaee8cc988eddfc591ca688baad
--- frontend/src/components/common/playlistItem.js
+++ frontend/src/components/common/playlistItem.js
@@ -1,28 +1,188 @@
-import React from 'react'
-import './playlistitem.css'
+import React from 'react';
+import './playlistitem.css';
+import styled from 'styled-components';
 function PlaylistItem() {
-return (
-
-            <section className="playlist">
-                <div className="container">
-                    <img src="https://cdn.vanguardngr.com/wp-content/uploads/2020/05/Wizkid-e1588973158415.jpg" alt="album" />
-                    <div className="playlistclass">
-                        <h3>Essence (ft Tems) - Wizkid</h3>
-                        <p>Added by smoothice</p>
-                    </div>
-                    <div className="para">
-                        <p>4.05 mins</p>
-                    </div>
-                    <div className="paragraph">
-                        <p>642 likes</p>
-                    </div>
-                    <div className="vector">
-                        <img className="vector1" src="https://png.pngitem.com/pimgs/s/111-1119299_black-hollow-heart-icon-hd-png-download.png" alt="heart" />
-                        <img className="vector2" src="https://img.flaticon.com/icons/png/512/17/17764.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF" alt="option" />
-                    </div>
-                </div>
-            </section>
-);
+  return (
+    <Playlist className='playlist'>
+      <Container>
+        <img
+          src='https://cdn.vanguardngr.com/wp-content/uploads/2020/05/Wizkid-e1588973158415.jpg'
+          alt='album'
+        />
+        <PlaylistClass className='playlistclass'>
+          <h3>Essence (ft Tems) - Wizkid</h3>
+          <p>Added by smoothice</p>
+        </PlaylistClass>
+        <Para className='para'>
+          <p>4.05 mins</p>
+        </Para>
+        <Paragraph className='paragraph'>
+          <p>642 likes</p>
+        </Paragraph>
+        <Vector className='vector'>
+          <img
+            className='vector1'
+            src='https://png.pngitem.com/pimgs/s/111-1119299_black-hollow-heart-icon-hd-png-download.png'
+            alt='heart'
+          />
+          <img
+            className='vector2'
+            src='https://img.flaticon.com/icons/png/512/17/17764.png?size=1200x630f&pad=10,10,10,10&ext=png&bg=FFFFFFFF'
+            alt='option'
+          />
+        </Vector>
+      </Container>
+    </Playlist>
+  );
 }
 
-export default PlaylistItem
+const Container = styled.div`
+  margin-top: 3em !important;
+  display: grid;
+  grid-template-columns: 14% 52% 20% 20%;
+  align-items: center;
+  padding-right: 1em;
+  padding-left: 1em;
+  padding-top: 2em;
+  width: 100%;
+  margin: auto;
+  box-sizing: border-box;
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+
+  @media (min-width: 1024px) {
+    margin-top: 3em !important;
+    display: grid;
+    grid-template-columns: 10% 45% 15% 15% 40%;
+    align-items: center;
+    padding-right: 1em;
+    padding-left: 1em;
+    padding-top: 2em;
+    width: 100%;
+    margin: auto;
+    box-sizing: border-box;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  }
+
+  @media (min-width: 1440px) {
+    margin-top: 3em !important;
+    display: grid;
+    grid-template-columns: 8% 45% 15% 15% 40%;
+    align-items: center;
+    padding-right: 1em;
+    padding-left: 1em;
+    padding-top: 2em;
+    width: 100%;
+    margin: auto;
+    box-sizing: border-box;
+    box-shadow: 0 5px 10px rgba(0, 0, 0, 0.2);
+  }
+`;
+
+const Para = styled.div`
+  display: none;
+
+  @media (min-width: 1024px) {
+    display: block;
+
+    p {
+      padding-bottom: 1em;
+      font-size: 14px;
+    }
+  }
+  @media (min-width: 1440px) {
+    display: block;
+
+    p {
+      padding-bottom: 1em;
+      font-size: 16px;
+    }
+  }
+`;
+const PlaylistClass = styled.div`
+  padding-bottom: 1em;
+
+  h3 {
+    font-family: 'Lato';
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 700;
+    letter-spacing: 0px;
+    text-align: left;
+  }
+
+  p {
+    font-size: 8px;
+    line-height: 2em;
+    font-weight: 400;
+    font-family: 'Lato';
+    font-size: 10px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 20px;
+    letter-spacing: 0px;
+    text-align: left;
+  }
+
+  @media (min-width: 1024px) {
+    h3 {
+      font-size: 14px;
+    }
+    p {
+      font-size: 12px;
+      line-height: 2.5em;
+    }
+
+    @media (min-width: 1440px) {
+      h3 {
+        font-size: 20px;
+      }
+      p {
+        font-size: 15px;
+        line-height: 2.5em;
+      }
+    }
+  }
+`;
+
+const Paragraph = styled.div`
+  p {
+    font-size: 9px;
+    padding-right: 3em;
+    padding-bottom: 1em !important;
+  }
+`;
+
+const Vector = styled.div`
+  .vector1 {
+    padding-right: 2em;
+    padding-bottom: 1em;
+    width: 43px;
+
+    @media (min-width: 1024px) {
+      padding-right: 2em;
+      padding-bottom: 1em;
+      width: 50px;
+    }
+    @media (min-width: 1440px) {
+      padding-right: 2em;
+      padding-bottom: 1em;
+      width: 50px;
+    }
+  }
+  .vector2 {
+    padding-bottom: 1em;
+
+    @media (min-width: 1024px) {
+      padding-right: 1em;
+      padding-bottom: 0.5em;
+      width: 20px;
+    }
+    @media (min-width: 1440px) {
+      padding-right: 1em;
+      padding-bottom: 0.5em;
+      width: 20px;
+    }
+  }
+`;
+
+export default PlaylistItem;
