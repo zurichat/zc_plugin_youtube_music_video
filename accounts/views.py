d369e136f3e7cd7a4d603b36b5d0cf591def351a
pauline-banye <paulinebanye@gmail.com>
2021-09-05 07:31:41 +0100

    pull frm dev

--- accounts/views.py
+++ accounts/views.py
@@ -15,20 +15,3 @@ class PluginInfo(View):
             "sidebar_url": "music.zuri.chat/sidebar",
         }
         return JsonResponse(data)
-from django.http import JsonResponse
-from django.views import View
-
-
-class PluginInfo(View):
-
-    def get(self, request):
-        data = {
-            "plugin_name": "Youtube Music Video Plugin",
-            "description": "This is a plugin that allows individuals in an organization to add music and video links from YouTube. These links are added to a shared playlist so that anyone in that organization can listen to or watch any of the shared videos or songs.",
-            "plugin_structure": "Monolith",
-            "team name": "Team Pythagoras",
-            "plugin_url": "music.zuri.chat",
-            "information_url": "music.zuri.chat/info",
-            "sidebar_url": "music.zuri.chat/sidebar",
-        }
-        return JsonResponse(data)
