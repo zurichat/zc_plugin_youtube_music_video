2043acf85366d734e3c3df2fb0385e43ae34f51e
classic-k <classicconceptone@gmail.com>
2021-09-05 03:58:11 -0700

    HttpResponse return

--- accounts/views.py
+++ accounts/views.py
@@ -95,7 +95,9 @@ class PluginInfo(View):
 class Test_report(View):
 
     def get(self, request):
-        return render(request,"report/report.html")
+
+        html_content = open("reports/report.html")
+        return  HttpResponse(html_content)       #render(request,"report/report.html")
 
 class SongsView(generics.ListAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin ):
 
