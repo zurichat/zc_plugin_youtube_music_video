fa8f5bd529492f7f0661b237246de9edae265893
JOSEPH <josepholukunle1107@gmail.com>
2021-09-05 09:36:08 +0100

    Setup the comments model urls for the comments endpoint of the plugin

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,10 @@
 from django.http import JsonResponse
 from django.views import View
+from rest_framework.response import Response
+from rest_framework import status, viewsets
+
+from .api import Comment
+from .serializers import CommentSerializer
 
 
 class PluginInfo(View):
@@ -15,3 +20,67 @@ class PluginInfo(View):
             "sidebar_url": "music.zuri.chat/sidebar",
         }
         return JsonResponse(data)
+
+
+# A dictionary created for the sake of passing dummy data
+# actual data would be gotten from the database when connected
+
+comments = {
+    1: Comment(id=1, username='Joseph', created_at='30 minutes ago', message='I like this music'),
+    2: Comment(id=2, username='Amara', created_at='35 minutes ago', message='This music is dope'),
+    3: Comment(id=3, username='Ken', created_at='40 minutes ago', message='This track is something else'),
+}
+
+
+class CommentViewSet(viewsets.ViewSet):
+    serializer_class = CommentSerializer
+
+    def list(self, request):
+        serializer = CommentSerializer(instance=comments.values(), many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            comments[max(comments) + 1] = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        try:
+            comment = comments[int(pk)]
+        except KeyError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except ValueError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = CommentSerializer(instance=comment)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        try:
+            comment = comments[int(pk)]
+        except KeyError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except ValueError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = CommentSerializer(instance=comment, data=request.data)
+        if serializer.is_valid():
+            comments[max(comments) + 1] = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        try:
+            comment = comments[int(pk)]
+        except KeyError:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        except ValueError:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+        serializer = CommentSerializer(instance=comment, data=request.data, partial=True)
+        if serializer.is_valid():
+            comments[max(comments) + 1] = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
