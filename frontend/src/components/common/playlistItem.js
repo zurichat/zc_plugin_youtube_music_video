f4820bd38d5908aa85b44e5fedac283493d0bd35
Temi-Pinheiro <temitopepinheiro@gmail.com>
2021-09-06 08:25:17 +0100

    style tweaks

--- frontend/src/components/common/playlistItem.js
+++ frontend/src/components/common/playlistItem.js
@@ -1,33 +1,35 @@
 // @ts-nocheck
 
-import React from "react";
-import styled from "styled-components";
+import React from 'react';
+import styled from 'styled-components';
 
-import Like from "./like";
+import Like from './like';
 
-import option from "../../media/option.svg";
+import option from '../../media/option.svg';
 
 function PlaylistItem(props) {
   const { title, addedBy, duration, liked, albumCover, onLike, likes } = props;
 
   return (
     <Wrapper>
-      <img src={albumCover} alt="album cover" className="item-albumCover" />
+      <img src={albumCover} alt='album cover' className='item-albumCover' />
 
-      <div className="item-info">
-        <div className="item-title">{title}</div>
-        <div className="item-addedBy">Added by {addedBy}</div>
+      <div className='item-info'>
+        <div className='item-title'>{title}</div>
+        <div className='item-addedBy'>
+          Added by <span>{addedBy}</span>
+        </div>
       </div>
 
-      <div className="item-group">
-        <div className="item-duration">{duration} mins</div>
-        <div className="item-like">{likes} likes</div>
-        <div className="item-icons">
+      <div className='item-group'>
+        <div className='item-duration'>{duration} mins</div>
+        <div className='item-like'>{likes} likes</div>
+        <div className='item-icons'>
           <Like liked={liked} onLike={onLike} />
           <img
             src={option}
-            alt="option img"
-            style={{ cursor: "pointer", width: "20px", height: "20px" }}
+            alt='option img'
+            style={{ cursor: 'pointer', width: '20px', height: '20px' }}
           />
         </div>
       </div>
@@ -39,19 +41,25 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   background: #fff;
-
-  transition: all 150ms ease-in-out;
-  margin-bottom: 3px;
+  font-family: 'Lato', sans-serif;
+  transition: all 200ms ease-in-out;
+  margin-bottom: 8px;
+  box-shadow: 0px 4px 6px rgba(0, 36, 24, 0.04);
+  height: 66px;
 
   &:hover {
-    box-shadow: 0 4px 6px rgba(0, 184, 124, 0.3);
+    box-shadow: 0 4px 6px rgba(0, 184, 124, 0.4);
+    /* transform: translateX(2px); */
   }
 
   .item-albumCover {
     display: block;
     margin-right: 20px;
-    width: 70px;
-    height: 100%;
+    margin-left: 10px;
+    width: 100%;
+    max-width: 50px;
+    flex-grow: 0;
+    height: 50px;
   }
 
   .item-info {
@@ -74,6 +82,10 @@ const Wrapper = styled.div`
 
   .item-addedBy {
     font-size: 13px;
+    span {
+      cursor: pointer;
+      text-decoration: underline;
+    }
   }
 
   .item-group {
@@ -89,6 +101,10 @@ const Wrapper = styled.div`
     align-items: center;
     justify-content: space-between;
     width: 60px;
+  }
+
+  .item-like {
+    color: rgba(153, 153, 153, 1);
   }
 
   @media screen and (max-width: 780px) {
