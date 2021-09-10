3108d12853b442bb06f0357a117ddd49998c7d1a
Okwuidegbe Emmanuel <okwuidegbeemmanuel@gmail.com>
2021-09-10 20:29:30 +0100

    Connect components to store

--- frontend/src/components/playlist.js
+++ frontend/src/components/playlist.js
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import { useSelector } from 'react-redux';
 
 import Player from './player';
 
@@ -11,6 +12,9 @@ import PlaylistItem from './common/playlistItem';
 import customCover from '../media/customCover.svg';
 
 import getSongs from '../mock-data/songs';
+
+import { selectAllSongs } from '../features/songsSlice';
+
 
 const custom = {
   id: '3kfkfk',
@@ -25,12 +29,14 @@ const custom = {
 function Playlist() {
   const [player, setPlayer] = useState(false);
   const [songs, setSongs] = useState([custom, ...getSongs()]);
+  const allSongs = useSelector(selectAllSongs);
 
   const handleLike = (song) => {
     const index = songs.indexOf(song);
     const list = [...songs];
     list[index] = { ...song, liked: !song.liked };
     setSongs(list);
+    dispatch(addSong(list))
   };
 
   return (
