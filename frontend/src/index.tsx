37091ccf2be2dcb459f6e94fdb6e9b4930f2be4d
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-15 09:48:39 +0100

    test setup 3

---
+++ frontend/src/index.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./store";
+
+import "./index.css";
+
+import App from "./App";
+
+ReactDOM.render(
+  <>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </>,
+  document.getElementById("root")
+);
