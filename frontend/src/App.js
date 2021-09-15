4a4e0912345111449db7080121a29dbb85fe158c
User <taofiqaiyelabegan45@gmail.com>
2021-09-15 11:58:54 +0800

    new change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Frontend Setup Test</h1>
-        <h3>Team Pythagoras</h3>
-        <h3>Team Pythagoras</h3>
-        <h3>Team Pythagoras</h3>
-        <h3>Team Pythagoras</h3>
-        <h3>Team Pythagoras</h3>
+        <h1>Ayoo</h1>
       </div>
     );
   }
