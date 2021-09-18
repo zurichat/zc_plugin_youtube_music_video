9e516ff3b98623838ff04f194fbd6bd6dff06c52
bankole <bankoleibrahim96@gmail.com>
2021-09-18 22:03:48 +0100

    function to get the like count

--- server/client/src/utils/likeCount.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import store from '../store/index';
-
-function getLikeCount(userId) {
-  return store.getState().likedSongs.find(e => e.id === userId).usersId.length;
-}
-
-export default getLikeCount;
