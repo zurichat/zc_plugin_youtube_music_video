e5837213577b0f59ef0023d79fb7d97e02464b44
bankole <bankoleibrahim96@gmail.com>
2021-09-18 20:48:31 +0100

    function to get the like count

--- server/client/dist/store/reducer.d.ts
+++ server/client/dist/store/reducer.d.ts
@@ -1,7 +1,14 @@
 declare const reducer: {
     users: import("redux").Reducer<import("../types/user").default[], import("redux").AnyAction>;
-    chats: import("redux").Reducer<any, import("redux").AnyAction>;
-    songs: import("redux").Reducer<any, import("redux").AnyAction>;
+    chats: import("redux").Reducer<{
+        id: string;
+        userId: string;
+        name: string;
+        time: number;
+        message: string;
+        avatar: string;
+    }[], import("redux").AnyAction>;
+    songs: import("redux").Reducer<import("../types/song").default[], import("redux").AnyAction>;
     likedSongs: import("redux").Reducer<import("../types/likedSong").default[], import("redux").AnyAction>;
     ui: import("redux").Reducer<{
         isLoading: boolean;
--- server/client/dist/store/songsSlice.d.ts
+++ server/client/dist/store/songsSlice.d.ts
@@ -1,6 +1,12 @@
 import { RootState } from ".";
+import Song from "../types/song";
 export declare const addSong: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, string>, removeSong: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, string>, updateSong: import("@reduxjs/toolkit").ActionCreatorWithPayload<any, string>;
-export declare const selectAllSongs: (state: RootState) => any;
-export declare const selectSongById: (state: RootState, songId: string) => any;
-declare const _default: import("redux").Reducer<any, import("redux").AnyAction>;
+export declare const songAction: {
+    dispatchAddSong: (payload: Song) => void;
+    dispatchRemoveSong: (payload: Song) => void;
+    dispatchUpdateSong: (payload: Song) => void;
+};
+export declare const selectAllSongs: (state: RootState) => Song[];
+export declare const selectSongById: (state: RootState, songId: string) => Song;
+declare const _default: import("redux").Reducer<Song[], import("redux").AnyAction>;
 export default _default;
