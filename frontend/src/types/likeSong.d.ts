b4be09cbed1ff6c46af9f91189538d5b95d87715
bankole <bankoleibrahim96@gmail.com>
2021-09-18 19:21:09 +0100

    getting the new setup

--- frontend/src/types/likeSong.d.ts
+++
@@ -1,4 +0,0 @@
-export default interface LikeSong {
-  userId: string;
-  songId: string;
-}
--- music/models.py
+++
@@ -1,5 +0,0 @@
-class Media:
-    def __init__(self, media_id, name, url):
-        self.media_id = media_id
-        self.name = name
-        self.url = url
