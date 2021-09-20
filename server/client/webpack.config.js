853f50fdebee4ed6fb646abe912a0145dc902b64
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-20 14:23:41 +0100

    hey hey

--- server/client/webpack.config.js
+++ server/client/webpack.config.js
@@ -1,7 +1,7 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
 // const webpack = require("webpack");
-const path = require("path");
+// const path = require("path");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
