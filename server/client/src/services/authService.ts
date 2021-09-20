d5b68604e4b8649b8205ebba9afbd68d8fb6deb3
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-20 22:06:40 +0100

    get cur user

--- server/client/src/services/authService.ts
+++ server/client/src/services/authService.ts
@@ -1,14 +1,21 @@
 // @ts-ignore
-// import { GetUserInfo } from "@zuri/zuri-control";
+import { GetUserInfo } from "@zuri/zuri-control";
+import axios from "axios";
 
 import avatar from "../media/chatItem.svg";
 
 function getCurrentUser() {
+  console.log(GetUserInfo());
   return {
     name: "user",
     id: "userId",
     avatar,
   };
+}
+
+function addToRoom() {
+  const endpoint = "http://localhost:8000/music/api/v1/add_to_room";
+  axios.post(endpoint, getCurrentUser().id);
 }
 
 const authService = { getCurrentUser };
