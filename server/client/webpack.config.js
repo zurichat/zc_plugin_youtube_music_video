ae24692bd360d3af45d475ca6241eb06fcf5bed9
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-20 14:20:16 +0100

    removed possible error cause

--- server/client/webpack.config.js
+++ server/client/webpack.config.js
@@ -1,5 +1,7 @@
 const { merge } = require("webpack-merge");
 const singleSpaDefaults = require("webpack-config-single-spa-react-ts");
+// const webpack = require("webpack");
+const path = require("path");
 
 module.exports = (webpackConfigEnv, argv) => {
   const defaultConfig = singleSpaDefaults({
@@ -11,5 +13,8 @@ module.exports = (webpackConfigEnv, argv) => {
 
   return merge(defaultConfig, {
     // modify the webpack config however you'd like to by adding to this object
+    optimization: {
+      minimize: true,
+    },
   });
 };
