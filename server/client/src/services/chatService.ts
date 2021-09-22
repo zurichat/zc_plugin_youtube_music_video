9824368999ec98ff83a8d4f681970016c908564a
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2021-09-22 17:49:07 +0100

    changed

--- server/client/src/services/chatService.ts
+++ server/client/src/services/chatService.ts
@@ -2,12 +2,19 @@ import axios from "axios";
 import { chatDispatch } from "../store/chatsSlice";
 import Chat from "../types/chat";
 
-const endpoint = "http://localhost:8000/music/api/v1/comments";
+const endpoint = "http://localhost:8000/music/api/v1/comments/";
 
 const getChats = async () => {
   try {
     const result = await axios.get(endpoint);
-    console.log(result);
+    /*let array = result.data.data;
+    3
+    let i = 0;
+    while (i !== result.data.data.lenght) {
+        chatDispatch.addChat(result.data.data[i])
+    }
+    */
+   console.log(result);
   } catch (e) {
     console.log(e.message);
   }
