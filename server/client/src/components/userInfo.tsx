a21e4319d744f6ec82589a8bcd723e30c3b49682
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-25 13:02:58 +0100

    changes

--- server/client/src/components/userInfo.tsx
+++ server/client/src/components/userInfo.tsx
@@ -1,10 +1,16 @@
+import { useSelector } from "react-redux";
+import { userDispatch, userSelect } from "../store/usersSlice";
+
 function UserInfo() {
+  const user = useSelector(userSelect.currentUser);
+
   const handleSetUser = () => {
     // @ts-ignore
     import("@zuri/zuri-control")
       .then(({ GetUserInfo }) => {
-        const { _id: id, first_name: name } = GetUserInfo();
-        console.log({ id, name });
+        const { _id: id, first_name: name, ...rest } = GetUserInfo();
+        console.log({ id, name, ...rest });
+        userDispatch.setCurrentUser({ ...user, id, name });
       })
       .catch((e) => console.log({ e }));
   };
