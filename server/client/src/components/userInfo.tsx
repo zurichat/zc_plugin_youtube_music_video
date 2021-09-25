d1c0223a6bb7a379ce6c28f34273d69c3150303c
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-25 13:12:21 +0100

    moving forward

--- server/client/src/components/userInfo.tsx
+++ server/client/src/components/userInfo.tsx
@@ -1,10 +1,11 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { userDispatch, userSelect } from "../store/usersSlice";
 
 function UserInfo() {
   const user = useSelector(userSelect.currentUser);
 
-  const handleSetUser = () => {
+  useEffect(() => {
     // @ts-ignore
     import("@zuri/zuri-control")
       .then(({ GetUserInfo }) => {
@@ -13,9 +14,7 @@ function UserInfo() {
         userDispatch.setCurrentUser({ ...user, id, name });
       })
       .catch((e) => console.log({ e }));
-  };
-
-  handleSetUser();
+  }, []);
 
   return (
     <div style={{ position: "fixed", bottom: "20px", display: "none" }}></div>
