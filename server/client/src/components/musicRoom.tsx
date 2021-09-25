e51576cb3898a9b73e1351222f4134798fa727dc
Pauline.B <63536707+pauline-banye@users.noreply.github.com>
2021-09-25 22:02:05 +0100

    Revert "Updated Chat UI and added message sent time in 12 hour format functionality to chat"

--- server/client/src/components/musicRoom.tsx
+++ server/client/src/components/musicRoom.tsx
@@ -10,6 +10,9 @@ function MusicRoom() {
       <div className="room-main">
         <Playlist />
       </div>
+      <div className="room-chat-container">
+        <Chat />
+      </div>
     </Wrapper>
   );
 }
@@ -23,6 +26,27 @@ const Wrapper = styled.div`
     overflow: hidden;
     position: relative;
     flex-grow: 1;
+    margin-right: 10px;
+  }
+
+  @media screen and (max-width: 1000px) {
+    justify-content: center;
+    align-items: center;
+
+    .room-main {
+      margin: 0;
+      flex-grow: 1;
+    }
+
+    .room-chat-container {
+      position: fixed;
+      top: 70px;
+      background: rgb(240, 240, 240);
+      flex-basis: 40%;
+      display: flex;
+      justify-content: center;
+      z-index: 111;
+    }
   }
 `;
 
