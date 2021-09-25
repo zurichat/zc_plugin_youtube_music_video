501b9b2c3507399839cf2821a6e21b3b68924eaa
jamido1 <66392050+jamido1@users.noreply.github.com>
2021-09-25 22:29:01 +0100

    room serializers

--- server/music/serializers.py
+++ server/music/serializers.py
@@ -2,17 +2,14 @@ from django.utils import timezone
 from rest_framework import serializers
 
 
-class UserSerializer(serializers.Serializer):
-    
-    user_ids = serializers.CharField(max_length=256)
-    user_name = serializers.CharField(max_length=256)
-    avatar = serializers.CharField(max_length=256)
+class MembersSerializer(serializers.Serializer):
+    user_id = serializers.CharField()
 
     def __str__(self):
         return str()
 
+
 class MediaSerializer(serializers.Serializer):
-    
     media_id = serializers.CharField()
     name = serializers.CharField()
     url = serializers.CharField()
@@ -20,29 +17,25 @@ class MediaSerializer(serializers.Serializer):
 
 
 class CommentSerializer(serializers.Serializer):
-    
     message = serializers.CharField(max_length=256)
-    time = serializers.IntegerField(max_value=None, min_value=None)
+    created_at = serializers.DateTimeField(default=timezone.now, read_only=True)
     userId = serializers.CharField(max_length=256)
-    name = serializers.CharField(max_length=256)
+    user_name = serializers.CharField(max_length=256)
     avatar = serializers.CharField(max_length=256)
-    
+
     # added_by = serializers.ListField(
     #     child=serializers.CharField(max_length=128), allow_null=False
     # )
-    
+
     def __str__(self):
         return str()
 
 
 class RoomSerializer(serializers.Serializer):
-    
-    org_id = serializers.CharField(max_length=128, required=True)
-    user_ids = UserSerializer(many=True)
+    org_id = serializers.CharField(required=True)
+    room_name = serializers.CharField()
+    description = serializers.CharField()
+    room_user_ids = serializers.ListField(child=MembersSerializer(many=True), allow_empty=False, required=True)
 
     def __str__(self):
         return str()
-
-
-
-
