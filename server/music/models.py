774c1ac70998e1b72a042361f9c04765f947f64e
Pauline.B <63536707+pauline-banye@users.noreply.github.com>
2021-09-26 09:54:36 +0100

    Revert "Rewrite views"

--- server/music/models.py
+++ server/music/models.py
@@ -3,23 +3,3 @@ class Media:
         self.media_id = media_id
         self.name = name
         self.url = url
-
-
-class RoomUser:
-    def __init__(self, _ids, name, avatar):
-    
-        self._ids = _ids
-        self.name = name
-        self.avatar = avatar
-
-
-class Room:
-    def __init__(self, _ids, room_name, description, type, members):
-
-        self._ids = _ids
-        self.room_name = room_name
-        self.description = description
-        self.type = type
-        self.members = members
-
-       
--- server/music/serializers.py
+++ server/music/serializers.py
@@ -3,15 +3,13 @@ from rest_framework import serializers
 
 
 class MembersSerializer(serializers.Serializer):
-    
-    user_id = serializers.CharField(max_length=50, read_only=True)
-    user_name = serializers.CharField(max_length=256, read_only=True)
-    avatar = serializers.CharField(max_length=256, required=False, read_only=True)
-    token = serializers.CharField(max_length=500, required=False, read_only=True)
+    user_id = serializers.CharField()
+    user_name = serializers.CharField()
+    avatar = serializers.CharField()
 
     def __str__(self):
         return str()
-    
+
 
 class MediaSerializer(serializers.Serializer):
     media_id = serializers.CharField()
@@ -21,17 +19,11 @@ class MediaSerializer(serializers.Serializer):
 
 
 class CommentSerializer(serializers.Serializer):
-    
-    _id = serializers.ReadOnlyField()
     message = serializers.CharField(max_length=256)
-    time = serializers.DateTimeField(default=timezone.now, read_only=True)
-    user_id = serializers.ListField(child=serializers.CharField(max_length=128), required=False, default=[])
-    
-    # message = serializers.CharField(max_length=256)
-    # time = serializers.IntegerField(max_value=None, min_value=None)
-    # userId = serializers.CharField(max_length=256)
-    # name = serializers.CharField(max_length=256)
-    # avatar = serializers.CharField(max_length=256)
+    time = serializers.IntegerField(max_value=None, min_value=None)
+    userId = serializers.CharField(max_length=256)
+    name = serializers.CharField(max_length=256)
+    avatar = serializers.CharField(max_length=256)
 
     # added_by = serializers.ListField(
     #     child=serializers.CharField(max_length=128), allow_null=False
@@ -40,45 +32,13 @@ class CommentSerializer(serializers.Serializer):
     def __str__(self):
         return str()
 
-    # def update(self, instance, validated_data):
-        
-    #     instance["_id"] = validated_data.get("_id", instance["_id"])
-    #     instance["message"] = validated_data.get("message", instance["message"])
-    #     instance["time"] = validated_data.get("time", instance["time"])
-    #     instance["user_id"] = validated_data.get("user_id", instance["user_id"])
-        
-    #     return instance
-
 
 class RoomSerializer(serializers.Serializer):
-    
-    _id = serializers.ReadOnlyField()
-    org_id = serializers.ReadOnlyField()
-    plugin_id = serializers.ReadOnlyField()
-    room_name = serializers.CharField(max_length=100)   
-    description = serializers.CharField(max_length=300, required=False)
-    room_url = serializers.CharField(required=False)
-    room_image = serializers.CharField(required=False)
-    type_of_room = serializers.CharField(max_length=50, required=True)
-    user_id = serializers.ListField(child=serializers.CharField(max_length=128), required=False, default=[])
+    org_id = serializers.CharField()
+    room_name = serializers.CharField()
+    room_url = serializers.CharField()
+    room_image = serializers.CharField()
+    description = serializers.CharField()
 
     def __str__(self):
         return str()
-
-
-class SongSerializer(serializers.Serializer):
-
-    _id = serializers.ReadOnlyField()
-    title = serializers.CharField(max_length=100, required=False)
-    duration = serializers.CharField(max_length=100)   
-    albumCover = serializers.CharField(max_length=300)
-    url = serializers.URLField(max_length=200, min_length=None, allow_blank=False)
-    # addedBy = UserSerializer(many=True, read_only=True)
-    # added_by = serializers.ListField(child=serializers.CharField(max_length=128), allow_empty=False, required=True)
-    added_by = serializers.ListField(child=serializers.CharField(max_length=128), required=False, default=[])
-    likedBy = serializers.NullBooleanField(required=False)
-
-    def __str__(self):
-        return str()
-
-    
