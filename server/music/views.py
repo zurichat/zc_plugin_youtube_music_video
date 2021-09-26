52241d5c7fcff8e0a46b5197af402960defcccda
pauline-banye <paulinebanye@gmail.com>
2021-09-26 09:46:36 +0100

    edited sidebar

--- server/music/views.py
+++ server/music/views.py
@@ -27,42 +27,96 @@ def check_if_user_is_in_room_and_return_room_id(user_id):
     return room_data["data"][0]["_id"]
 
 
-def get_room_info(room_id=None):
-    room_data = read_data(settings.ROOM_COLLECTION)
-    output = {
-        "name": room_data["data"][0]["name"],
-        "description": room_data["data"][0]["Description"],
-        "image": "#",
-        "room_url": "https://music.zuri.chat/music/api/v1/room/614f5562cf2c0f1ad7584fa0"
-    }
-    return output
+# def get_room_info(room_id=None):
+#     room_data = read_data(settings.ROOM_COLLECTION)
+#     output = {
+#         "name": room_data["data"][0]["name"],
+#         "description": room_data["data"][0]["Description"],
+#         "image": "#",
+#         "room_url": "https://music.zuri.chat/music/api/v1/room/614f5562cf2c0f1ad7584fa0"
+#     }
+#     return output
 
 
-class SidebarView(GenericAPIView):
+# class SidebarView(GenericAPIView):
 
+#     def get(self, request, *args, **kwargs):
+#         org_id = request.GET.get('org', None)
+#         user_id = request.GET.get('user', None)
+
+#         pub_room = get_room_info()
+
+#         data = {
+
+#             "message": "Plugin Sidebar Retrieved",
+#             "data": {
+#                 "name": "Music Plugin",
+#                 "description": "Plays YouTube Links",
+#                 "organisation_id": org_id,
+#                 "plugin_id": "613ceb50ceee2ab59d44df2f",
+#                 "user_id": user_id,
+#                 "group_name": "Music",
+#                 "show_group": True,
+#                 "public_rooms": pub_room,
+#                 "joined_rooms": [],
+#             },
+#             "success": "true"
+#         }
+#         return JsonResponse(data, safe=True)
+
+
+class SidebarView(APIView):
+    
     def get(self, request, *args, **kwargs):
-        org_id = request.GET.get('org', None)
-        user_id = request.GET.get('user', None)
 
-        pub_room = get_room_info()
+        org = request.GET.get('org', None)
+        user = request.GET.get('user', None)
+        room = settings.ROOM_COLLECTION
+        plugin_id = settings.PLUGIN_ID
+        org_id = settings.ORGANIZATON_ID
 
-        data = {
+        if request.GET.get('org') and request.GET.get('user'):
+            url = f'https://api.zuri.chat/organizations/{org}/members/{user}'
+            headers = {
+                "Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb29raWUiOiJNVFl6TWpZME16VXhOM3hIZDNkQlIwUlplRTVFWnpGT1JGRXpXbFJTYVUxdFJteFpiVmswV2xkTk5GbDZhM2xOVVQwOWZLaXFkd3RkaFJlOUdpYUgxZ0dQWXpKLVRFTUc4Qm9ZNnIyNUJNQ2pHWlNnIiwiZW1haWwiOiJ1Y2hpd2FsbGkuYkBnbWFpbC5jb20iLCJpZCI6IjYxNDg1NDQ3ZTRiMmFlYmY4ZWM4YzkyMSIsIm9wdGlvbnMiOnsiUGF0aCI6Ii8iLCJEb21haW4iOiIiLCJNYXhBZ2UiOjc5Mzk3ODU3MjUsIlNlY3VyZSI6ZmFsc2UsIkh0dHBPbmx5IjpmYWxzZSwiU2FtZVNpdGUiOjB9LCJzZXNzaW9uX25hbWUiOiJmNjgyMmFmOTRlMjliYTExMmJlMzEwZDNhZjQ1ZDVjNyJ9.YznvgpGNmf9GqnBYBgHYcJucMk3oNLKQf11McWYSwb0",
+                "Content-Type" : "application/json",
+                }
+            r = requests.get(url,headers=headers)
+            print(r.status_code)
 
-            "message": "Plugin Sidebar Retrieved",
-            "data": {
-                "name": "Music Plugin",
-                "description": "Plays YouTube Links",
-                "organisation_id": org_id,
-                "plugin_id": "613ceb50ceee2ab59d44df2f",
-                "user_id": user_id,
-                "group_name": "Music",
-                "show_group": True,
-                "public_rooms": pub_room,
-                "joined_rooms": [],
-            },
-            "success": "true"
-        }
-        return JsonResponse(data, safe=True)
+            if r.status_code == 200:
+                public_url = f"https://api.zuri.chat/data/read/{plugin_id}/{room}/{org_id}"
+
+                r = requests.get(public_url)
+                return JsonResponse(r, safe=True)
+               
+            else:
+                return JsonResponse({
+                    "name": "Music Plugin",
+                    "description": "Plays YouTube Links",
+                    "plugin_id": plugin_id,
+                    "organisation_id": org,
+                    "user_id": user,
+                    "group_name": "Music",
+                    "show_group": False,
+                    "public_rooms":[],
+                    "joined_rooms":[]
+                    })
+        else:
+            return JsonResponse({
+                   "name": "Music Plugin",
+                    "description": "Plays YouTube Links",
+                    "plugin_id": plugin_id,
+                    "organisation_id": org,
+                    "user_id": user,
+                    "group_name": "Music",
+                    "show_group": False,
+                    "public_rooms":[],
+                    "joined_rooms":[]
+                    })
+
+def is_valid(param):
+    return param != "" and param is not None
 
 
 class PluginInfoView(GenericAPIView):
@@ -220,7 +274,7 @@ class AddToRoomView(APIView):
     @staticmethod
     def get_obj_id_and_append_user_id(request):
         room_data = read_data(settings.ROOM_COLLECTION)
-        user_ids = room_data["data"][0]["room_user_ids"]
+        user_ids = room_data["data"][0]["user_ids"]
         _id = room_data["data"][0]["_id"]
         if request.data["id"] not in user_ids:
             user_ids.append(request.data["id"])
@@ -234,7 +288,7 @@ class AddToRoomView(APIView):
         _id, user_ids = self.get_obj_id_and_append_user_id(request)
 
         payload = {
-            "room_user_ids": user_ids
+            "user_ids": user_ids
         }
 
         data = write_data(settings.ROOM_COLLECTION, object_id=_id, payload=payload, method="PUT")
@@ -254,6 +308,7 @@ class AddMember(GenericAPIView):
     serializer_class = MembersSerializer
 
     def post(self, request):
+
         user_id = request.query_params.get('user')
         user_name = request.query_params.get('display name')
         avatar = request.query_params.get('profile picture')
@@ -327,48 +382,6 @@ class DeleteMember(GenericAPIView):
                     else:
                         return user_list
             return user_list
-
-
-# # #removes a single user id from the members collection
-# # @api_view(['GET', 'POST'])
-# # def leave_room(request):
-#     plugin_id = settings.PLUGIN_ID
-#     organization_id = settings.ORGANIZATON_ID
-#     collection_name = settings.MEMBERS_COLLECTION
-    
-#     member_data = read_data(settings.MEMBERS_COLLECTION)
-#     # _id = member_data["data"][0]["room_user_ids"]
-#     _id = member_data["room_user_ids"]
-
-#     if request.method == 'GET':
-#         data = read_data(collection_name)
-#         return Response(data)
-
-#     elif request.method == 'PUT':
-
-#         url = "https://api.zuri.chat/data/write"
-
-#         payload = {
-#             "plugin_id": plugin_id,
-#             "organization_id": organization_id,
-#             "collection_name": collection_name,
-#             "bulk_delete": False,
-#             "object_id": _id,
-#             "filter": {}
-#         }
-
-#         try:
-#             r = requests.post(url, data=json.dumps(payload))
-#             # Note: use only {"_id": ""} in the payload
-
-#             if r.status_code == 200:
-#                 return Response({"message": "User left room"},
-#                                 status=status.HTTP_200_OK)
-#             else:
-#                 return Response({"error": r.json()['message']}, status=r.status_code)
-
-#         except exceptions.ConnectionError as e:
-#             return Response(str(e), status=status.HTTP_502_BAD_GATEWAY)
 
 
 
