4f98990870bf275dc83cdc40a9910d366a5bd349
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-26 22:41:43 +0100

    changed baseUrl

--- server/client/src/services/httpService.ts
+++ server/client/src/services/httpService.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:8000/music/api/v1";
-axios.defaults.baseURL = "https://music.zuri.chat/music/api/v1/";
+// axios.defaults.baseURL = "https://music.zuri.chat/music/api/v1/";
+axios.defaults.baseURL = "159.65.123.65/music/api/v1/";
 
 const endpoints = {
   songEndpoint: "song/",
