853cd402f0f6c141c561d002886f11485c501086
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-09-26 16:03:45 +0100

    removed slashes

--- server/client/src/services/httpService.ts
+++ server/client/src/services/httpService.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 
 // axios.defaults.baseURL = "http://localhost:8000/music/api/v1";
-axios.defaults.baseURL = "https://music.zuri.chat/music/api/v1";
+axios.defaults.baseURL = "https://music.zuri.chat/music/api/v1/";
 
 const endpoints = {
-  songEndpoint: "/song",
-  commentEndpoint: "/comments",
-  likeEndpoint: "/like",
-  userCountEndpoint: "/header-user-count",
-  addToRoom: "/add_to_room",
-  leaveEndpoint: "/exit",
+  songEndpoint: "song",
+  commentEndpoint: "comments",
+  likeEndpoint: "like",
+  userCountEndpoint: "header-user-count",
+  addToRoom: "add_to_room",
+  leaveEndpoint: "exit",
 };
 
 const httpService = {
