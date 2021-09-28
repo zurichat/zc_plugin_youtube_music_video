653f9df67df5a66db9d5ed3625f417aa2e11847f
Pauline.B <63536707+pauline-banye@users.noreply.github.com>
2021-09-28 10:21:53 +0100

    Update httpService.ts

--- server/client/src/services/httpService.ts
+++ server/client/src/services/httpService.ts
@@ -5,13 +5,13 @@ axios.defaults.baseURL = "https://music.zuri.chat/music/api/v1/";
 // axios.defaults.baseURL = "159.65.123.65/music/api/v1/";
 
 const endpoints = {
-  songEndpoint: "song/",
-  commentEndpoint: "comments/",
-  likeEndpoint: "like/",
-  userCountEndpoint: "header-user-count/",
-  addToRoom: "add_to_room/",
-  leaveEndpoint: "exit/",
-  roomEndpoint: "room/",
+  songEndpoint: "songs",
+  commentEndpoint: "comments",
+  likeEndpoint: "likesong",
+  userCountEndpoint: "user-count",
+  addToRoom: "add_to_room",
+  leaveEndpoint: "exit",
+  roomEndpoint: "room",
 };
 
 const httpService = {
