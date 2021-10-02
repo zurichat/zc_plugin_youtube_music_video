17abd4be693c9dcf33f3634646553e0d673fecaf
Pauline.B <63536707+pauline-banye@users.noreply.github.com>
2021-10-02 10:33:16 +0100

    Update serializers.py

--- server/music/serializers.py
+++ server/music/serializers.py
@@ -41,7 +41,8 @@ class CommentSerializer(serializers.Serializer):
     userId = serializers.CharField(max_length=256, required=False)
     name = serializers.CharField(max_length=256, required=False)
     avatar = serializers.CharField(max_length=256, required=False)
-    time = serializers.DateTimeField(read_only=True)
+    #time = serializers.DateTimeField(read_only=True)
+    time = serializers.IntegerField()
 
     def create(self, validated_data):
         return Comment(**validated_data)
