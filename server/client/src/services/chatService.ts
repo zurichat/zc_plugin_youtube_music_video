7ce547d1a7f1dc212d39ad57c766541db41c6f59
Sherlock-HolmesJM <sherllyj191@gmail.com>
2021-10-09 08:38:33 +0100

    saved previous changes

--- server/client/src/services/chatService.ts
+++ server/client/src/services/chatService.ts
@@ -19,20 +19,18 @@ const addChat = async (chat: Chat) => {
   const newChat: any = { ...chat };
   delete newChat.id;
 
-  chatDispatch.addChat({ ...chat, notSent: true});
-  console.log("Dispatched message");
+  chatDispatch.addChat({ ...chat, notSent: true });
 
   try {
-    await httpService.post(commentEndpoint, newChat, {timeout: 15000});
-    chatDispatch.sentChat({...chat});
-
+    await httpService.post(commentEndpoint, newChat, { timeout: 15000 });
+    chatDispatch.sentChat({ ...chat });
 
     const { chats } = store.getState();
 
     chats.slice(0, chats.length - 8).forEach(({ id }) => deleteChat(id));
   } catch (error) {
     console.log("Chat error:", error.message);
-    chatDispatch.failChat({...chat});
+    chatDispatch.failChat({ ...chat });
   }
 
   return;
